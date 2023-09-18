b6c57f6692cca6be6a2cc3d2ec522e8b78351c9d
Sandrikk <krevovasandra@gmail.com>
2023-09-18 11:21:35 +0200

    added filter section with component Filter.svelte

---
+++ client/src/components/Filter.svelte
@@ -0,0 +1,19 @@
+
+<!-- Filter.svelte -->
+<script>
+    export let title;
+</script>
+
+<label>{title}:</label>
+<div class="filter-checkboxes">
+
+    <label>
+        <input type="checkbox"/>English
+    </label>
+
+</div>
+
+<style>
+
+</style>
+
---
+++ client/src/components/FilterSection.svelte
@@ -0,0 +1,18 @@
+<!-- FilterSection.svelte -->
+<script>
+    import Filter from "./Filter.svelte";
+</script>
+
+<div class="filter-section">
+    <!-- Language Filter -->
+    <Filter title="Language"/>
+
+</div>
+
+<style>
+    label {
+        display: block;
+    }
+</style>
+
+
