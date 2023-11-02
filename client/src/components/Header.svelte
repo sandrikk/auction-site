a3b3ce443d05b12f7fbdbfce00fcc5fd4ad68b72
Sandrikk <krevovasandra@gmail.com>
2023-11-02 17:33:52 +0100

    header add won auctions

--- client/src/components/Header.svelte
+++ client/src/components/Header.svelte
@@ -22,11 +22,11 @@
     <ul class="menu">
         <li><a class:active={active === "/books"} href="/books">Books</a></li>
         <li><a class:active={active === "/users"} href="/users">Users</a></li>
-        {#if $tokenStore === null}
-            <li><a class="login" class:active={active === "/login"} href="/login">Login</a></li>
-        {:else}
+        {#if $tokenStore}
             <li><a class:active={active === "/users/me/won"} href="/users/me/won">Won Bids</a></li>
             <li><a class="logout" on:click={handleLogout}>Logout</a></li>
+        {:else}
+            <li><a class="login" class:active={active === "/login"} href="/login">Login</a></li>
         {/if}
     </ul>
 </nav>
